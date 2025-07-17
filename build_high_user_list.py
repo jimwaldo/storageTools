fd3b437ea0a8f5a79b6843803fc67c226c7c8f32
jimwaldo <jiniwaldo@gmail.com>
2025-07-17 08:15:24 -0400

    Clean-up of build_high_user_list.py, changing the list of top 20 users from printing to stdout to creating a csv file, and adding the role of the person to the report.

---
+++ build_high_user_list.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import csv
+import sys
+import pickle
+
+
+def get_top_20_users(data_list, field_index):
+    """
+    Sort and return top 20 users based on specified field
+    """
+    sorted_list = sorted(data_list, key=lambda x: float(x[field_index]) if x[field_index] != '' else 0.0, reverse=True)
+    return sorted_list[:20]
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('Usage: python build_high_user_list.py input.pkl output.csv field_index')
+        sys.exit(1)
+
+    try:
+        with open(sys.argv[1], 'rb') as fin:
+            data = pickle.load(fin)
+
+        field_index = int(sys.argv[3])
+        top_users = get_top_20_users(data, field_index)
+
+        out_list = []
+
+        out_list.append(["Top 20 users by field index {field_index}".format(field_index=field_index)])
+        out_list.append(['Name', 'Email', 'Role','Storage Value'])
+        for user in top_users:
+            out_list.append([user[2], user[3], user[6], user[field_index]])
+        with open(sys.argv[2], 'w', encoding='utf-8') as fout:
+            cout = csv.writer(fout)
+            cout.writerows(out_list)
+
+    except FileNotFoundError:
+        print(f"Error: File {sys.argv[1]} not found")
+        sys.exit(1)
+    except ValueError:
+        print("Error: Field index must be a valid integer")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        sys.exit(1)
